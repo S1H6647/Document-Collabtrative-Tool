a88a6f5cab2f9beda0286567cf9ef99ff2287792
Sanjit Hyanju <sanjithyanju99@gmail.com>
2026-03-17 11:57:28 +0545

    chore: initial project setup with Spring Boot and Maven

---
+++ src/main/java/com/project/realtimedoccollab/RealTimeDocCollabApplication.java
@@ -0,0 +1,13 @@
+package com.project.realtimedoccollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDocCollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeDocCollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/realtimedoccollab/RealTimeDocCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.realtimedoccollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDocCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
